4a3d9284a31dad4ac2a4c1acde29f9fee3caeccd
John Isaac Delgado <ji23delgado@gmail.com>
2021-03-31 09:42:56 +0800

    Adjusted based on YSL 1.5.2

--- server.py
+++ server.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template
 from holo_assist import HoloAssist
 import os
 
-email = os.environ.get('TEST_EMAIL')
-passwd = os.environ.get('TEST_PASS')
-hta = HoloAssist('channel ids.txt', email, passwd)
+hta = HoloAssist('channel ids.txt')
 hta.close_browser()
 
 app = Flask(__name__)
@@ -16,11 +14,14 @@ def index():
 @app.route('/like')
 def like():
     hta.close_browser()
+    hta.is_streaming()
     hta.start_liking_with_data("isaac", "localhost", "DevAisha23!", "YSL", "stream_data",
-                               "C:/Users/ISAAC/PycharmProjects/videoLikerYoutube2.0/Stream data")
+                               "C:/Users/ISAAC/PycharmProjects/videoLikerYoutube2.0/Stream data",
+                               'C:/Users/ISAAC/AppData/Roaming/Mozilla/Firefox/Profiles/fwnbfuph.default-release',
+                               'C:/Program Files (x86)/geckodriver.exe', ['--mute-audio'])
     hta.open_holotools()
     hta.clear_data()
     return render_template('like.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.1.12')
+    app.run(debug=True, host='192.168.1.10')
