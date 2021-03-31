d21106a5b9eb738a50b7a89276f18a3c5fc72518
John Isaac Delgado <ji23delgado@gmail.com>
2021-03-31 09:42:13 +0800

    Adjusted code for YSL 1.5.2

--- holo_assist.py
+++ holo_assist.py
@@ -1,4 +1,4 @@
-from YSL import StreamLiker
+from ysl.YSL import StreamLiker
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,13 +10,13 @@ import time
 
 
 class HoloAssist(StreamLiker):
-    def __init__(self, channels_path, email, passwd):
-        super(HoloAssist, self).__init__(channels_path, email, passwd)
+    def __init__(self, channels_path):
+        super(HoloAssist, self).__init__(channels_path)
         self.link = None
 
     def open_holotools(self):
         print()
-        self.link = 'https://hololive.jetri.co/#/watch?videoId=' + ",".join(self.active_streams)
+        self.link = 'https://hololive.jetri.co/#/watch?videoId=' + ",".join(self.currently_streaming.values())
 
         if self.link == 'https://hololive.jetri.co/#/watch?videoId=':
             print("No streamers are currently streaming.")
@@ -71,65 +71,3 @@ class HoloAssist(StreamLiker):
     @staticmethod
     def close_browser():
         os.system("TASKKILL /F /IM firefox.exe")
-
-    def send_email(self):
-        self.driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-        self.driver.get("https://mail.google.com/mail/u/0/#inbox")
-
-        compose = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id=":bv"]/div/div'))
-        )
-
-        ActionChains(self.driver).move_to_element(compose).click(compose).perform()
-
-        to = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '/html/body/div[23]/div/div/div/div[1]/div[3]/div[1]/div[1]/div/div/div/div[3]/div/div/div[4]/table/tbody/tr/td[2]/form/div[1]/table/tbody/tr[1]/td[2]/div/div/textarea'))
-        )
-
-        ActionChains(self.driver).move_to_element(to).click(to).perform()
-
-        to.send_keys("dev.isaac23@gmail.com")
-
-        sub = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH,
-                                        '/html/body/div[23]/div/div/div/div[1]/div[3]/div[1]/div[1]/div/div/div/div[3]/div/div/div[4]/table/tbody/tr/td[2]/form/div[3]/div/input'))
-        )
-
-        ActionChains(self.driver).move_to_element(sub).click(sub).perform()
-
-        sub.send_keys("Test")
-
-        msg = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH,
-                                        '/html/body/div[23]/div/div/div/div[1]/div[3]/div[1]/div[1]/div/div/div/div[3]/div/div/div[4]/table/tbody/tr/td[2]/table/tbody/tr[1]/td/div/div[1]/div[2]/div[1]/div/table/tbody/tr/td[2]/div[2]/div'))
-        )
-
-        ActionChains(self.driver).move_to_element(msg).click(msg).perform()
-
-        msg.send_keys("Hi")
-
-        send = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH,
-                                        '/html/body/div[23]/div/div/div/div[1]/div[3]/div[1]/div[1]/div/div/div/div[3]/div/div/div[4]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/div/div/div[4]/table/tbody/tr/td[1]/div/div[2]'))
-        )
-
-        ActionChains(self.driver).move_to_element(send).click(send).perform()
-
-
-
-if __name__ == "__main__":
-    email = os.environ.get('TEST_EMAIL')
-    passwd = os.environ.get('TEST_PASS')
-    hta = HoloAssist('channel ids.txt', email, passwd)
-    hta.close_browser()
-    hta.start_liking_with_data("isaac", "localhost", "DevAisha23!", "YSL", "stream_data",
-                              "C:/Users/ISAAC/PycharmProjects/videoLikerYoutube2.0/Stream data")
-    hta.open_holotools()
-    hta.clear_data()
-
-    # hta = HoloAssist(email, passwd, sl.number_of_active_streams)
-    # hta.config_driver('C:/Program Files (x86)/geckodriver.exe', ['--mute-audio'])
-    # hta.link = 'https://hololive.jetri.co/#/watch?videoId=' + ','.join(sl.streams_liked_id)
-    # hta.open_holotools()
-    # hta.clear_data()
-    # hta.driver_quit()
